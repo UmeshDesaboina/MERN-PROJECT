57eedcd9118632564154c407eda4c135478c6bf8
UmeshDesaboina <149916679+UmeshDesaboina@users.noreply.github.com>
2025-10-21 18:18:00 +0530

    Remove unused CORS import from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-const cors = require("cors");
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 import Header from './components/Header';
